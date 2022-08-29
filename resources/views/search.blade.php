142cd691100fee50a59a4efe2a8c36517e7ab3a7
jandales <jesusandales021993@gmail.com>
2022-08-30 00:12:06 +0800

    update

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,7 +2,7 @@
 @section('content') 
     <div class="container"> 
         <div class="productPage mt-4">    
-            <div class="search-page mb-2">
+            <div class="search-page mb-5">
                 <h2>Search results</h2> 
                 <form  action="{{ route('search') }}" method="get">
                     <div class="input-search">                        
@@ -10,8 +10,11 @@
                         <span class="close-search"><i class="fa fa-search" aria-hidden="true"></i></span>
                     </div>
                 </form>  
-            </div>      
-            <x-product-list-component :products="$products"/>
+            </div>  
+            @if($keyword != null) 
+                <x-product-list-component :products="$products"/> 
+            @endif   
+           
         </div>
     </div>  
 @endsection
